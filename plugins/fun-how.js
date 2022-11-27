de7363a648734bcc7a89c8aff5aa70bcee02405c
ShooC <118227993+ShooC@users.noreply.github.com>
2022-11-27 20:24:54 +0700

    Update fun-how.js

--- plugins/fun-how.js
+++ plugins/fun-how.js
@@ -9,6 +9,6 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 }
 handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
 handler.tags = ['kerang', 'fun']
-handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
+handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy|lesbi|sadgirl|uwu|cute|tai|guoblok|ngentot|buaya|titis|kntl)/i
 
 export default handler
