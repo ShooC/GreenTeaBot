65ebe61898b3908aa52f1b0386b8ba1d853f42a8
ShooC <118227993+ShooC@users.noreply.github.com>
2022-11-28 14:11:19 +0700

    Update xp-profile.js

--- plugins/xp-profile.js
+++ plugins/xp-profile.js
@@ -10,20 +10,19 @@ let handler = async (m, { conn }) => {
     let name = conn.getName(who)
     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
-┏━━━━ꕥ〔 *PROFILE* 〕ꕥ━━━⬣
-┃🌿 • *Name:* ${name}
-┃🍵 • *Username:* ${registered ? name : ''}
-┃📧 • *Tag:* @${who.replace(/@.+/, '')}
-┃📞 • *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-┃🔗 • *Link:* https://wa.me/${who.split`@`[0]}
-┃🪐 • *Age:* ${registered ? age : ''} Tahun
-┗━ꕥ
-┏━ꕥ${readMore} 
-┃🌟 • *Premium:* ${premium ? "Tidak" :"Iya"}
-┃⏰ • *Waktu Prem:* 
+Profile Kamu
+${readMore} 
+ • *Name:* ${name}
+ • *Username:* ${registered ? name : ''}
+ • *Tag:* @${who.replace(/@.+/, '')}
+ • *Nomor:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+ • *Wame:* https://wa.me/${who.split`@`[0]}
+ • *Umur:* ${registered ? age : ''} Tahun
+ • *Premium:* ${premium ? "Tidak" :"Iya"}
+ • *Waktu Prem:* 
 ${clockString(user.premiumTime - new Date() * 1)}
-┃📑 • *Registered:* ${registered ? 'Ya': 'Tidak'}
-┗━━━━━━━━ꕥ`.trim()
+ • *Terdaftar:* ${registered ? 'Ya': 'Tidak'}
+`.trim()
     conn.sendButton(m.chat, str, botdate, pp, [[`${registered ? 'Menu':'Verify'}`, `${user.registered ? '.menu':'.verify'}`]], fkon, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
 }
 handler.help = ['profile [@user]']
@@ -43,5 +42,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return ['┃ ', ye, ' *Tahun 🗓️*\n', '┃ ', mo, ' *Bulan 🌙*\n', '┃ ', d, ' *Hari ☀️*\n', '┃ ', h, ' *Jam 🕐*\n', '┃ ', m, ' *Menit ⏰*\n', '┃ ', s, ' *Detik ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
+  return ['┃ ', ye, ' *Tahun 🗓️*\n', '', mo, ' *Bulan 🌙*\n', '', d, ' *Hari ☀️*\n', '', h, ' *Jam 🕐*\n', '', m, ' *Menit ⏰*\n', '', s, ' *Detik ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
 }
